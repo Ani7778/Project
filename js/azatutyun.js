099038ea88eb3fad7c1cc4f7adda6118ba96623b
ASUS-ПК\ASUS <aniasatryan336@gmail.com>
2021-01-04 18:24:49 +0400

    js code in external file

--- js/azatutyun.js
+++ js/azatutyun.js
@@ -1,13 +1,23 @@
 var navbar_options = document.getElementById("myNavbar");
 var hidden = navbar_options.classList.contains("hidden_block");
-document.getElementsByClassName("dropbtn").addEventListener("mouseover", showHideNavbaroptions);
 
+document.getElementsByClassName("dropbtn")[0].addEventListener("mouseover", showHideNavbaroptions);
+document.getElementsByClassName("icon")[0].addEventListener("click", showHideNavbaroptions);
+
+    
 function showHideNavbaroptions() {
+    
     if (hidden) {
-    	console.log('Show Navbar');
-    	navbar_options.className = "navbar_items_hover responsive";
+        console.log("Show Navbar");
+        navbar_options.className = "navbar_items_hover responsive";
     } else {
         console.log("Hide Navbar");
-    	navbar_options.className = "navbar_items_hover hidden_block";
+        navbar_options.className = "navbar_items_hover hidden_block";
     }
 }
+
+
+
+
+
+
